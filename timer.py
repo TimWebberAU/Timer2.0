e33b38023d470e1d1a3066b7fe0e6e7c2d33d543
Tim Webber <twebber@hotmail.com.au>
2023-12-28 17:14:56 +1030

    Initial commit

---
+++ timer.py
@@ -0,0 +1,7 @@
+#
+#    Author: Tim W
+#      Date: 28th Dec 2023
+#   Project: Creation of a timer app for fun and utility. I use timers for
+#            productivity and haven't enjoyed the web-based or Mac varieties.
+# 
+
