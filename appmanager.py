f093279b95b7870e441fbbfb316e8381e18bfe63
Tim Webber <twebber@hotmail.com.au>
2024-01-04 18:50:06 +1030

    Handling User Input
    Experimented with taking user keyboard input and filtering that.
    
    That created significant work.
    
    Located a spinner widget in Kivy which limits what users can do.
    
    Began first implementation of 3 spinner widgets (h/m/s).

--- appmanager.py
+++ appmanager.py
@@ -12,6 +12,7 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.label import Label
 from kivy.uix.button import Button
+from kivy.uix.spinner import Spinner
 from input import UserInput, UserInputInterface
 from timer import TimerDisplay, TimerDisplayInterface
 
@@ -24,36 +25,57 @@ class TimerLayout(GridLayout):
 
         # The following isn't correct - Instantiation should occur one step back
         # and the objects are passed in but reference the interfaces - Liskov
-        # I'm also instantiating the parent classes here - the interfaces
+        # I'm also instantiating the abstract classes here - the interfaces
         # self.__user_input_object = UserInputInterface()
         # self.__timer_display_object = TimerDisplayInterface()
 
-        self.cols = 1
+        self.cols = 1   # Outer Grid
+
+        self.internal_layout = GridLayout(cols=3, spacing=10)
 
         self.add_widget(Label(text="ENTER A TIME"))
 
-        self.user_input_hours = TextInput(hint_text="HOURS (0-24)", multiline=False, input_filter=self.filter_hours)
-        self.add_widget(self.user_input_hours)
-        self.user_input_minutes = TextInput(hint_text="MINUTES",multiline=False)
-        self.add_widget(self.user_input_minutes)
-        self.user_input_seconds = TextInput(hint_text="SECONDS",multiline=False)
-        self.add_widget(self.user_input_seconds)
+        self.internal_layout.add_widget(Label(text="HOURS (0-24)"))
+        self.internal_layout.add_widget(Label(text="MINUTES (0-60)"))
+        self.internal_layout.add_widget(Label(text="SECONDS (0-60)"))
+
+        self.hours_spinner = Spinner(text="0",
+                               values=("0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+                                       "10", "11", "12", "13", "14", "15", "16", "17", 
+                                       "18", "19", "20", "21", "22", "23", "24"),
+                               size_hint=(None, None),
+                               size=(80, 40))
+        self.internal_layout.add_widget(self.hours_spinner)
+
+        self.minutes_spinner = Spinner(text="0",
+                               values=("0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+                                       "10", "11", "12", "13", "14", "15", "16", "17", "18",
+                                       "19", "20", "21", "22", "23", "24", "25", "26", "27",
+                                       "28", "29", "30", "31", "32", "33", "34", "35", "36",
+                                       "37", "38", "39", "40", "41", "42", "43", "44", "45",
+                                       "46", "47", "48", "49", "50", "51", "52", "53", "54",
+                                       "55", "56", "57", "58", "59", "60"),
+                               size_hint=(None, None),
+                               size=(80, 40))
+        self.internal_layout.add_widget(self.minutes_spinner)
+
+        self.seconds_spinner = Spinner(text="0",
+                               values=("0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+                                       "10", "11", "12", "13", "14", "15", "16", "17", "18",
+                                       "19", "20", "21", "22", "23", "24", "25", "26", "27",
+                                       "28", "29", "30", "31", "32", "33", "34", "35", "36",
+                                       "37", "38", "39", "40", "41", "42", "43", "44", "45",
+                                       "46", "47", "48", "49", "50", "51", "52", "53", "54",
+                                       "55", "56", "57", "58", "59", "60"),
+                               size_hint=(None, None),
+                               size=(80, 40),
+                               pos_hint={"center_x": .5, "center_y": .5})
+        self.internal_layout.add_widget(self.seconds_spinner)
+
+        self.add_widget(self.internal_layout)   # Add internal grid to outer grid
 
         self.submit_button = Button(text="Begin", font_size=20)
-
         self.add_widget(self.submit_button)
-    
-
-    def filter_hours(self, input_text, from_undo=False):
-        """Assess correct input for hours."""
-
-        if input_text.isdigit() and 0 <= int(input_text) <= 24:
-            return input_text
-        
-    
-# CREATE A CLASS FOR OVERRIDING TEXTINPUT
-# https://kivy.org/doc/stable/api-kivy.uix.textinput.html#filtering
-
 
 
 class TimerApp(App):
