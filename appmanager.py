63ec0b6a7d3b7237dd74f4fa561f3fa3222f0b57
Tim Webber <twebber@hotmail.com.au>
2024-01-09 18:24:59 +1030

    Seconds timer functionality im plemented.
    Used Kivy's Clock class to schedule the timer.
    
    Have only completed for secon
    ds so far.
    
    Display is also too basic currently.

--- appmanager.py
+++ appmanager.py
@@ -10,7 +10,7 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
-from time import sleep
+from kivy.clock import Clock
 from input import UserInput, UserInputInterface
 from timer import TimerDisplay, TimerDisplayInterface
 
@@ -22,24 +22,30 @@ class TimerLayout(Widget):
     minutes = ObjectProperty(None)
     seconds = ObjectProperty(None)
 
+
         # The following isn't correct - Instantiation should occur one step back
         # and the objects are passed in but reference the interfaces - Liskov
         # I'm also instantiating the abstract classes here - the interfaces
         # self.__user_input_object = UserInputInterface()
         # self.__timer_display_object = TimerDisplayInterface()
     
-    
+
     def begin_btn(self):
         print(f"Hours: {int(self.hours.value)}, Minutes: {int(self.minutes.value)}, Seconds: {int(self.seconds.value)}")
 
-    # KIVY DOESNT LIKE SLEEP FUNCTION - REPLACE WITH KIVY.CLOCK INSTEAD ********
-    def timer_display(self):
-        timer_seconds = int(self.seconds.value)
+        self.timer_seconds = int(self.seconds.value)    # Create trackable variable
+        Clock.schedule_interval(self.timer_display, 1)  # Kivy scheduler, 1 sec interval
 
-        for i in range(timer_seconds, 0, -1):
-            print(i)
-            self.ids.timer.text = str(i)
-            sleep(1)
+
+    def timer_display(self, dt_time_elapsed):
+
+        self.ids.timer.text = str(self.timer_seconds)   # Update GUI timer
+
+        if self.timer_seconds <= 0:
+            Clock.unschedule(self.timer_display)
+        
+        self.timer_seconds -= 1
+
 
 
 class TimerApp(App):
