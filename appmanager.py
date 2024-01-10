a177eddfd8b7e5c7548030be61c25f068f135264
Tim Webber <twebber@hotmail.com.au>
2024-01-10 18:52:38 +1030

    Building algo for timer.
    Algo will be more complicated than I thought.
    
    May need to iterate on this.

--- appmanager.py
+++ appmanager.py
@@ -33,18 +33,41 @@ class TimerLayout(Widget):
     def begin_btn(self):
         print(f"Hours: {int(self.hours.value)}, Minutes: {int(self.minutes.value)}, Seconds: {int(self.seconds.value)}")
 
-        self.timer_seconds = int(self.seconds.value)    # Create trackable variable
-        Clock.schedule_interval(self.timer_display, 1)  # Kivy scheduler, 1 sec interval
+        Clock.unschedule(self.timer_display)            # Remove existing schedule
+        self.timer_seconds = int(self.seconds.value)    # Create trackable SEC variable
+        self.timer_minutes = int(self.minutes.value)    # Create trackable MIN variable
+        self.timer_hours = int(self.hours.value)        # Create trackable HR variable
+
+        # Ensure timer only starts if a value exists
+        if self.timer_seconds > 0 or self.timer_minutes > 0 or self.timer_hours > 0:
+            Clock.schedule_interval(self.timer_display, 1)  # Kivy scheduler, 1 sec interval
 
 
     def timer_display(self, dt_time_elapsed):
+                        
+        self.ids.timer.text = self.timer_format()       # Update GUI timer
 
-        self.ids.timer.text = str(self.timer_seconds)   # Update GUI timer
+        if self.timer_seconds == 0:
 
-        if self.timer_seconds <= 0:
+            if self.timer_minutes > 0:
+                self.timer_minutes -= 1
+                self.timer_seconds = 60
+            
+            if self.timer_minutes == 0:
+
+                if self.timer_hours > 0:
+                    self.timer_hours -= 1
+                    self.timer_minutes = 59
+                    self.timer_seconds = 60
+
+        if self.timer_seconds <= 0 and self.timer_minutes <= 0 and self.timer_hours <= 0:
             Clock.unschedule(self.timer_display)
         
         self.timer_seconds -= 1
+    
+
+    def timer_format(self):
+        return f"Hr: {str(self.timer_hours)}  Min: {str(self.timer_minutes)}  Sec: {str(self.timer_seconds)}"
 
 
 
